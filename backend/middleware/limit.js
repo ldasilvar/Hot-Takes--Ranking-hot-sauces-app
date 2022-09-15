4e9c9b6947b5b8d891742bea1f44ab8ac3a8ce92
ldasilvar <ldasilvar@gmail.com>
2022-09-15 16:51:46 +0100

    fixed IP limiter

--- backend/middleware/limit.js
+++ backend/middleware/limit.js
@@ -1,8 +1,8 @@
 const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
-    max: 200,
-    windowMs: 60 * 60 * 1000,
+    max: 300,
+    windowMs: 5 * 60 * 1000,
     message: "Too many request from this IP"
 });
 
