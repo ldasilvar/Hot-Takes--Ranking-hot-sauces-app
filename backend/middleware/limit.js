7593a6e2c5204997e69eac4d223feb43d84bd440
ldasilvar <ldasilvar@gmail.com>
2022-09-21 12:51:40 +0100

    auth

--- backend/middleware/limit.js
+++ backend/middleware/limit.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 //Limits the actions on the website from any one IP to 400 every five minutes to protect website against brute attacks
 const limiter = rateLimit({
-    max: 400,
+    max: 600,
     windowMs: 5 * 60 * 1000,
     message: "Too many request from this IP"
 });
